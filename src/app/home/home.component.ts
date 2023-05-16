2d0d0b41f9a52fb86fe1c940004a40df33fa0b59
Alexandre Carneiro <alexandre.carneiro@new-work.se>
2023-05-16 15:28:59 +0200

    feat: adds blinking css to prompt text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private _router: Router) {}
+
+  @HostListener('window:keydown.enter', ['$event'])
+  handleKeyDown() {
+    this._router.navigate(['game']);
+  }
+}
