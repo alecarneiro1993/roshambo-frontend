aabe5bc72952baac4eb57d97fa864bb28d1c3573
Alexandre Carneiro <alexandre.carneiro@new-work.se>
2023-05-16 17:07:01 +0200

    chore(game-title): prettier fixes

--- src/app/shared/components/game-title/game-title.component.ts
+++ src/app/shared/components/game-title/game-title.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-game-title',
   templateUrl: './game-title.component.html',
-  styleUrls: ['./game-title.component.sass']
+  styleUrls: ['./game-title.component.sass'],
 })
-export class GameTitleComponent {
-
-}
+export class GameTitleComponent {}
