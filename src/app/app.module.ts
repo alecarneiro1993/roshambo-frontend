ad947967ef03ae0664a1768529701382c130bc04
Alexandre Carneiro <alexandre.carneiro@new-work.se>
2023-05-16 15:04:05 +0200

    feat: initial code for home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeModule } from './home';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HomeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
