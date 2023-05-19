c94a406fa3e08f7e7387fff20acaa30bd0192948
Alexandre Carneiro <alexandre.carneiro@new-work.se>
2023-05-20 01:46:17 +0200

    test: rewrites AppComponent spec in testing library

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,39 +1,13 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { render, screen } from '@testing-library/angular';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() =>
-    TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientModule],
-      declarations: [AppComponent],
-    })
-  );
-
-  describe('page content', () => {
-    it('should create the app', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      expect(app).toBeTruthy();
-    });
-
-    it(`should have as title 'roshambo-frontend'`, () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.componentInstance;
-      expect(app.title).toEqual('roshambo-frontend');
-    });
-
-    it('should render title and subtitle', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const compiled = fixture.nativeElement as HTMLElement;
-      expect(compiled.querySelector('.game-title')?.textContent).toContain(
-        'Roshambo'
-      );
-      expect(compiled.querySelector('.subtitle')?.textContent).toContain(
-        'Street Fighter Edition'
-      );
+  fdescribe('page content', () => {
+    it('should render title and subtitle', async () => {
+      await render(AppComponent, {});
+      expect(screen.getByText('Roshambo'));
+      expect(screen.getByText('Street Fighter Edition'));
     });
   });
 });
